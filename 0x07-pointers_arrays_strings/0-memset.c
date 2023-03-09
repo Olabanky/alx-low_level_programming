b4192ab4cd7b4da205ccb5f2193ff7cd3df2a9fe
Olabanky <bankole.azeezo@gmail.com>
2023-03-09 08:06:44 +0100

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -43,39 +43,22 @@ void simple_print_buffer(char *buffer, unsigned int size)
                 }
 
                 printf("0x%02x", buffer[i]);
-
-                i++;
-
-        }
-
-        printf("\n");
-
+		i++;
+	}
+	printf("\n");
 }
-
-
 
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
-
 int main(void)
-
 {
-
-    char buffer[98] = {0x00};
-
-
-
-    simple_print_buffer(buffer, 98);
-
-    _memset(buffer, 0x01, 95);
-
-    printf("-------------------------------------------------\n");
-
-    simple_print_buffer(buffer, 98);    
-
-    return (0);
-
+	char buffer[98] = {0x00};
+	simple_print_buffer(buffer, 98);
+	_memset(buffer, 0x01, 95);
+	printf("-------------------------------------------------\n");
+	simple_print_buffer(buffer, 98);
+	return (0);
 }
