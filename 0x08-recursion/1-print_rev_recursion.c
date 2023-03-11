d7fc8935c3a886d917cadb8508667578f591da8e
Olabanky <bankole.azeezo@gmail.com>
2023-03-11 08:45:58 +0100

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/*
+ *_print_rev_recursion - A function that prints string in reverse
+ *@s: character value
+ *if statement: check for the end of string and add a new line
+ *else statement: prints each character in reversal
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar('\n')
+	else
+	{
+		_print_rev_recursion(++s);
+		_putchar(*s);
+	}
+}
