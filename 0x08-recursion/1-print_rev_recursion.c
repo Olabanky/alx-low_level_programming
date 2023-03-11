ed849f692dd07922422a6759cfe0a6043885147f
Olabanky <bankole.azeezo@gmail.com>
2023-03-11 09:34:48 +0100

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -3,13 +3,13 @@
 /**
  *_print_rev_recursion - A function that prints string in reverse
  *@s: character value
- *if statement: check for the end of string and add a new line
+ *if statement: check for the end of string
  *else statement: prints each character in reversal
  */
 void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
-		_putchar('\n');
+		return;
 	else
 	{
 	_print_rev_recursion(s + 1);
