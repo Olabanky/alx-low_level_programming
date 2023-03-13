bfa60822e63063c42fd293b9b935badfaf031a7e
Olabanky <bankole.azeezo@gmail.com>
2023-03-13 04:57:34 +0100

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,32 +3,32 @@
 
 /**
  *_sqrt - Recursively compares given number to the square of various numbers
- *@n: integer
- *@y: Integer
- *Return: -
+ *@n: base integer
+ *@y: iterator integer
+ *Return: y
  */
 
 int _sqrt(int n, int y)
 {
 	if (n == y * y)
 		return (y);
-	else if (n > y)
+	else if (n >= y * y)
 		return (_sqrt(n, ++y));
 	else
 		return (-1);
 }
 
 /**
- *_sqrt_recursion - A function that return natural square root
- *@n: integer
- *@y: Integer
- *Return: -1 if n has no natural square root
+ *_sqrt_recursion - A function that return natural square root of n
+ *@n: base integer
+ *Return: square root of n
  */
-
 int _sqrt_recursion(int n)
 {
-	int _sqrt(int n, int y);
-	int y = 1;
-
-	return (_sqrt(n, y));
+	if (n < 0)
+		return (-1);
+	else if (n == 1 || n == 0)
+		return (n);
+	else
+		return (_sqrt(n, 2));
 }
