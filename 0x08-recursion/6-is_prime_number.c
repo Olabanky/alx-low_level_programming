3e4f8f331e5ebc34bf6be155e1bc5687853bb089
Olabanky <bankole.azeezo@gmail.com>
2023-03-13 05:27:16 +0100

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,34 @@
+#include <main.h>
+
+
+/**
+ *number - checks if input integer is prime number
+ *@n: integer
+ *@y: integer
+ *Return: -
+ */
+int number(int n, int y)
+{
+	if (y == 1 || y == 0)
+		return (0);
+	else if (n % y == 0)
+		return (1);
+	else
+		return (number(n, y - 1));
+}
+
+/**
+ *is_prime_number - checks if input integer is prime number
+ *@n: integer
+ *Return: 1 if prime number, otherwise 0
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	else if (n == 2)
+		return (1);
+	else
+		return (!number(n, n - 1));
+}
